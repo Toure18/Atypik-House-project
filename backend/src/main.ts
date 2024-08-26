482b1dc92248dccd8a64c2aac70b15473661bd52
Madjoula Toure <madjoula@MacBook-Air-de-Madjoula.local>
2024-08-26 10:09:58 +0200

    fix cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,11 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*', 
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept',
-    credentials: true,
+    origin: '*',
   });
+  
 
   app.useGlobalPipes(new ValidationPipe());
 
