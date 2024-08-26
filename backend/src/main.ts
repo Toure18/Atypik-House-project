20db4bfb4a36855e4ca720284cf119827d3d29ee
Madjoula Toure <madjoula@MacBook-Air-de-Madjoula.local>
2024-08-26 06:49:17 +0200

    fix cors problem

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,16 +3,15 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
     origin: ['https://f2i-dsp4-g3-mt-mz-ai-admin.web.app', 'https://f2i-dsp4-g3-mt-mz-ai-f42d7.web.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
     credentials: true,
   });
-  
 
   app.useGlobalPipes(new ValidationPipe());
 
@@ -23,11 +22,11 @@ async function bootstrap() {
     .addTag('AtypicHouse')
     .build();
 
-  const document = SwaggerModule.createDocument(app,config );
+  const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document, {
     jsonDocumentUrl: 'swagger/json',
   });
-  
+
   await app.listen(5000);
 }
 bootstrap();
